f4f540d2a686a7a30e7cd6c2fdf6af32535b6173
Nafees-Syed <syednafees1997@gmail.com>
2021-11-22 20:28:40 -0500

    Added the dropdown codes

--- app/src/main/java/com/example/teamproject/SignUpActivity.java
+++ app/src/main/java/com/example/teamproject/SignUpActivity.java
@@ -4,23 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText email, password, confirmPassword;
     Button continueBtn;
+    Spinner dropdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        dropdown = findViewById(R.id.spinner);
         email = findViewById(R.id.emailEditText);
         password = findViewById(R.id.passwordEditText);
         confirmPassword = findViewById(R.id.confirmPasswordEditText);
         continueBtn = findViewById(R.id.continueButton);
+
+        String[] items = new String[]{"Donor", "Food Bank"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
 
         continueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
