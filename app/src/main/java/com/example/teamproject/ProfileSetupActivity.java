a37a15ada1e8c163de3088c7780de4ff7021240d
Nafees-Syed <syednafees1997@gmail.com>
2021-11-24 22:45:09 -0500

    commit to commit

--- app/src/main/java/com/example/teamproject/ProfileSetupActivity.java
+++ app/src/main/java/com/example/teamproject/ProfileSetupActivity.java
@@ -9,6 +9,6 @@ public class ProfileSetupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_setup_activity);
+        setContentView(R.layout.activity_profile_setup);
     }
 }
