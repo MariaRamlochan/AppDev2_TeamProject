a45ba803da02bbb39ebb2d06b486eddc8d9755d2
Nafees-Syed <syednafees1997@gmail.com>
2021-11-26 01:55:32 -0500

    Creation of the Testing Main page for testing purposes

---
+++ app/src/main/java/com/example/teamproject/TestingPage.java
@@ -0,0 +1,14 @@
+package com.example.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing_page);
+    }
+}
