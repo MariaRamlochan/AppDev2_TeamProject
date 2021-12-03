4c2fae543d4eaa5f7db8e2fe59687e83e25e457e
Nafees-Syed <syednafees1997@gmail.com>
2021-12-03 00:14:51 -0500

    creation of the navs

--- app/src/main/java/com/example/teamproject/NavigationDrawer.java
+++ app/src/main/java/com/example/teamproject/NavigationDrawer.java
@@ -33,19 +33,13 @@ public class NavigationDrawer extends AppCompatActivity {
         email = findViewById(R.id.userEmailTextView);
 
         setSupportActionBar(binding.appBarNavigationDrawer.toolbar);
-        binding.appBarNavigationDrawer.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_aboutUs, R.id.nav_donations, R.id.nav_settings)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
